76f69ec3384f57a52f52693741dc11038605d667
taofikarianto <taofikarianto@gmail.com>
2023-04-29 05:12:56 +0700

    add type data

--- bundle/index.js
+++ bundle/index.js
@@ -3,3 +3,17 @@ let namagw = "Taofik Arianto";
 let username = 123;
 console.log({ namagw });
 console.log({ username });
+// Ordinary Array
+let mhs;
+mhs = ["Mata", "Fikar"];
+console.log({ mhs });
+// tuple array
+let member;
+member = ["Jamal", 25, true];
+console.log({ member });
+let book;
+book = {
+    author: "Raditya Dika",
+    isSold: true,
+};
+console.log({ book });
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,30 @@ let username: number = 123;
 
 console.log({ namagw });
 console.log({ username });
+
+// Ordinary Array
+let mhs: string[];
+mhs = ["Mata", "Fikar"];
+console.log({ mhs });
+
+// tuple array
+let member: [string, number, boolean];
+member = ["Jamal", 25, true];
+console.log({ member });
+
+//TIPE DATA CUSTOM
+
+type BookType = {
+  author: string;
+  isSold: boolean;
+  price?: number;
+};
+
+let book: BookType;
+
+book = {
+  author: "Raditya Dika",
+  isSold: true,
+};
+
+console.log({ book });
