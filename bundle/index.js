be9a0397111f275644dcf52429e173acfb95211e
taofikarianto <taofikarianto@gmail.com>
2023-04-28 17:04:02 +0700

    add ts-basic

---
+++ bundle/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let namagw = "Taofik Arianto";
+let username = 123;
+console.log({ namagw });
+console.log({ username });
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let namagw: string = "Taofik Arianto";
+
+let username: number = 123;
+
+console.log({ namagw });
+console.log({ username });
